781cb9a943ffb7df6ba76357dd4e7d07944641fe
Michael Bloch <michaelbloch1991@gmail.com>
2018-04-14 15:28:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Michael Bloch"
-var height = 820
+var height = "820"
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height + 1)
+console.log(parseInt(height) + 1)
